bfdf7be91cdd62db49e2676c54ff483060d33bc9
Gerard Hynes <gkhynes@gmail.com>
2021-06-21 02:03:42 +0100

    Build out recipe pages

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ recipes }) {
         {recipes?.length > 0 &&
           recipes.map((recipe) => (
             <li key={recipe._id} className="recipe-card">
-              <Link href="/">
+              <Link href={`/recipes/${recipe.slug.current}`}>
                 <a>
                   <img src={urlFor(recipe.mainImage).url()} alt={recipe.name} />
                   <span>{recipe.name}</span>
