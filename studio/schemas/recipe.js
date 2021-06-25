017a88dfe3d3903b6144ce8307f7a5323c2476b8
Gerard Hynes <gkhynes@gmail.com>
2021-06-25 21:50:38 +0100

    Add recipe summary

--- studio/schemas/recipe.js
+++ studio/schemas/recipe.js
@@ -9,6 +9,11 @@ export default {
       type: "string"
     },
     {
+      name: "summary",
+      title: "Recipe summary",
+      type: "string"
+    },
+    {
       name: "slug",
       title: "Slug",
       type: "slug",
